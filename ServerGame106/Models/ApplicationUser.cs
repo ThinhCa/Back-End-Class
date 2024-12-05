15417e2826b8a5bfea0ba38b2ce4882c690ddc1e
ASUS <thinhzomv4@gmail.com>
2024-12-05 23:20:52 +0700

    Add project files.

---
+++ ServerGame106/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace ServerGame106.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        [ForeignKey("Region")]
+
+        public int RegionId { get; set; }
+        public string? Avatar {  get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+        public string OTP { get; set; }
+    }
+}
